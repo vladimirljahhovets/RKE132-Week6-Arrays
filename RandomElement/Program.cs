34e1d26193089610e4f238fb6889b353ef444ab7
Vladimir Ljahhovets <vladimir.ljahhovets@tktk.ee>
2024-05-17 21:23:59 +0300

    week6 code

---
+++ RandomElement/Program.cs
@@ -0,0 +1,8 @@
+﻿
+string[] snacks = { "sushi", "pizza", "burger", "chicken wings", "asian takeaway" };
+
+Random rnd = new Random();
+int randomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonight we are going to eat {snacks[randomIndex]}.");
+
---
+++ Week6StringAndArrays/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+string hello = "hello, world! Here I am?";
+
+int stringLength = hello.Length;
+
+
+hello  = hello.Trim();
+char firstLetter = hello[0];
+
+Console.WriteLine(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+
+
+
+firstLetter = hello[0];
+
+Console.WriteLine(firstLetter);
+
+Console.WriteLine(hello);
+
+
+
+//for (int i = 0; i < hello.Length; i++)
+//{
+//    if (char.IsWhiteSpace(hello[i]))
+//    { wordCounter++; }
+//}
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter + 1} words");
+
+
